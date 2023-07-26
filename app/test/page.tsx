871fa19a30d39a3f7be7671029e46f048c060ebe
Prakash Rai <prakash.bahing21@gmail.com>
2023-07-26 20:26:59 +0545

    Feat:Static metadata

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title:"Testing page",
+  description:"This is description page"
+}
+
+const test = () => {
+  return <div>test</div>;
+};
+
+export default test;
